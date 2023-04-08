33a0019e6c972af8de728cca3edf59c784299d48
Sergey Ilin <ilin_sergey90@mail.ru>
2023-04-08 13:02:05 +0300

    change func answer

--- utils.py
+++ utils.py
@@ -71,7 +71,8 @@ def answer(query: str):
         response = {}
         response['dataset'] = salary_list
         response['labels'] = time_list
-        return response
+        json_answer = json.dumps(response)
+        return json_answer
     correct_answer = '{"dt_from": "2022-09-01T00:00:00", \
 "dt_upto": "2022-12-31T23:59:00", \
 "group_type": "month"}'
